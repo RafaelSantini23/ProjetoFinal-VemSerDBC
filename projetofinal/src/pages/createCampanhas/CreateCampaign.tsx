ea4f1fa666c9f79c7f4083181feada703dc4ed05
Alisson Manica <alissonmanica.aai@gmail.com>
2022-04-25 20:26:32 -0300

    feat: pull das alterações feitas.

--- projetofinal/src/pages/createCampanhas/CreateCampaign.tsx
+++ projetofinal/src/pages/createCampanhas/CreateCampaign.tsx
@@ -1,5 +1,5 @@
 import { Field, Form, Formik, FormikHelpers } from "formik";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { connect, DispatchProp } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import * as Yup from 'yup';
@@ -9,42 +9,49 @@ import { FundraiserDTO } from "../../models/FundraiserDTO";
 import { RootState } from "../../store";
 import { isLoggedin, validDate } from "../../utils/Utils";
 import { createCampaign } from "../../store/actions/fundraiserAction";
-
+import CreatableSelect from 'react-select/creatable';
+import moment from "moment";
 
 
 function CreateCampaign({ campaign, dispatch }: FundraiserDTO & DispatchProp) {
-   const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        isLoggedin(navigate)
-    }, [])
+  const handleChange = (value: any, setFieldValue: any) => {
+    let list = value.map((item: any) => item.value)
+    setFieldValue('categories', list)
+  };
+
+    
+  useEffect(() => {
+      isLoggedin(navigate)
+  }, [])
 
 
-    const SignupSchema = Yup.object().shape({
-      goal: Yup.string()
-      .min(4, "Pelo menos 4 números!")
-      .required('Campo Obrigatório!'),
+  const SignupSchema = Yup.object().shape({
+    goal: Yup.string()
+    .min(4, "Pelo menos 4 números!")
+    .required('Campo Obrigatório!'),
 
-      validdate: Yup.string()
-      .min(10, 'Data inválida!')
-      .test('Data válida!', 'Data inválida!', (value) => validDate(value))
+    validdate: Yup.string()
+    .min(10, 'Data inválida!')
+    .test('Data válida!', 'Data inválida!', (value) => validDate(value))
 
-      .required('Campo Obrigatório!'),
+    .required('Campo Obrigatório!'),
 
-      automaticClose: Yup.boolean()
-      .oneOf([true , false], 'Campo Obrigatório!')
-      .nullable(),
+    automaticClose: Yup.boolean()
+    .oneOf([true , false], 'Campo Obrigatório!')
+    .nullable(),
 
-      title: Yup.string()
-      .required('Campo Obrigatório!'),
+    title: Yup.string()
+    .required('Campo Obrigatório!'),
 
-      description: Yup.string()
-      .required('Campo Obrigatório!'),
+    description: Yup.string()
+    .required('Campo Obrigatório!'),
 
-      categories: Yup.string()
-      .min(3, 'Categoria minima de 3 letras!')
-      .required('Campo Obrigatório!'),
-    });
+    categories: Yup.array()
+    .min(1, 'Campo Obrigatório!')
+    .required('Campo Obrigatório!'),
+  });
 
 
   return (
@@ -64,13 +71,14 @@ function CreateCampaign({ campaign, dispatch }: FundraiserDTO & DispatchProp) {
                   }}
                   validationSchema={SignupSchema}
                 onSubmit={(
-                    values,
+                    values: FundraiserDTO['campaign'],
                     { setSubmitting }: FormikHelpers<FundraiserDTO['campaign']>
                     ) => {        
+                      console.log(values.categories)
                       const campaign = {
                         title: values.title,
-                        goal: values.goal,
-                        validdate: values.validdate,
+                        goal: values.goal as number,
+                        validdate: moment(values.validdate).format('YYYY-MM-DD'),
                         automaticClose: values.automaticClose,
                         coverPhoto: values.coverPhoto,
                         categories: values.categories,
@@ -126,7 +134,7 @@ function CreateCampaign({ campaign, dispatch }: FundraiserDTO & DispatchProp) {
                       </DivValidate>
                       <DivValidate>
                           <LabelForm htmlFor='categories'>Categorias da campanha</LabelForm>
-                          <InputStyle name="categories" id="categories"  placeholder="Digite a(s) categoria(s) da campanha" />
+                          <Field component={CreatableSelect} isMulti="true" onChange={(event: any) => handleChange(event, props.setFieldValue)} name="categories" id="categories" placeholder="Digite a(s) categoria(s)" />
                           {props.errors.categories && props.touched.categories ? (
                             <SpanError>{props.errors.categories}</SpanError>
                             ) : null}
--- projetofinal/src/store/actions/fundraiserAction.tsx
+++ projetofinal/src/store/actions/fundraiserAction.tsx
@@ -4,11 +4,21 @@ import api from "../../api";
 import { FundraiserDTO } from "../../models/FundraiserDTO";
 
 
-export const createCampaign = async (dispatch: AppDispatch, values: FundraiserDTO['campaign'], navigate: NavigateFunction ) => {
+export const createCampaign = async (dispatch: AppDispatch, values: any, navigate: NavigateFunction ) => {
   
+    const formData = new FormData()
+
+    formData.append('title', values.title)
+    formData.append('goal', values.goal)
+    formData.append('validdate', values.validdate)
+    formData.append('automaticClose', values.automaticClose)
+    formData.append('coverPhoto', values.coverPhoto)
+    formData.append('categories', values.categories)
+    formData.append('description', values.description)
+
   try {
       
-      const { data } = await api.post('/fundraiser', values);
+      const { data } = await api.post('/fundraiser/save', formData);
       
       const campaign = {
           type: 'SET_CAMPAIGN',
